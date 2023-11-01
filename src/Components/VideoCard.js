ce1a8c24020e0047dffc9c328a08a9bcef16fb73
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2023-11-01 11:10:19 +0530

    coments added

--- src/Components/VideoCard.js
+++ src/Components/VideoCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const VideoCard = ({ info }) => {
   //const { snippet, statistics } = info;
   //const { channelTitle, thumbnails, title } = snippet;
-  console.log(info);
+
   return (
     <div className="p-2 m-2 w-72 shadow-lg">
       <img
--- src/Components/WatchPage.js
+++ src/Components/WatchPage.js
@@ -2,27 +2,31 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../Utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
+import Like from "./Like";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
-
-  console.log(searchParams.get("v"));
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(closeMenu());
   });
   return (
-    <div className="px-5">
-      <iframe
-        width="1000"
-        height="500"
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+    <div className=" flex flex-col ">
+      <div className="px-5">
+        <iframe
+          width="1000"
+          height="500"
+          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <Like />
+      <CommentsContainer />
     </div>
   );
 };
