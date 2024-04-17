494c8f18cf6b6071436448921ff209330693a1d4
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-04-17 21:28:27 +0530

    small modification done

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -2,6 +2,9 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../Utils/appSlice";
 import { TOGGLE_LOGO, USERACCOUTN, YOUTUBELOGO } from "../Utils/constant";
+import { CiSearch } from "react-icons/ci";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { FaUserCircle } from "react-icons/fa";
 
 const Head = () => {
   const dispatch = useDispatch();
@@ -11,7 +14,7 @@ const Head = () => {
   };
 
   return (
-    <div className="grid  w-[800px] mx-20 sm:mx-0 sm:w-full grid-flow-col p-2 m-2 shadow-lg ">
+    <div className="grid  bg-white w-[800px] mx-20 sm:mx-0 sm:w-full grid-flow-col p-2 m-2 shadow-lg sticky top-0 z-30">
       <div className="flex col-span-1  ">
         <img
           onClick={() => toggleMenuHandler()}
@@ -35,34 +38,26 @@ const Head = () => {
           placeholder="Search"
         />
         <button className="border border-gray-400 p-2 rounded-r-full bg-gray-100 h-12 ">
-          Search
+          <CiSearch className="w-5 h-5" />
         </button>
-        <div className=" bg-gray-100 h-12 w-12 rounded-3xl ml-3 cursor-pointer">
+        <div className=" bg-gray-100 h-12 w-12 rounded-full mx-2 flex justify-center items-center  cursor-pointer">
           <img
-            className=" h-7 m-3 bg-gray-100 w-7 rounded-2xl pb-1 br-2"
-            src="https://www.svgrepo.com/show/445247/mic-solid.svg"
+            className="  bg-gray-100 w-8 h-8 rounded-2xl flex justify-center items-center "
+            src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-25-512.png"
             alt="mic"
           />
         </div>
       </div>
 
-      <div className=" col-span-2 flex">
+      <div className=" col-span-2 flex gap-3  items-center">
         <img
-          className="h-10 hidden sm:block w-12 pt- mt-2 bg-none cursor-pointer mx-8 hover:bg-gray-100 rounded-3xl"
+          className="h-10 hidden sm:block w-12 pt- mt-2 bg-none cursor-pointer  hover:bg-gray-100 rounded-3xl"
           src="https://cdn.iconscout.com/icon/premium/png-512-thumb/create-4911912-4083720.png?f=webp&w=256"
           alt="create_logo"
         />
-        <img
-          className="h-9 mt-3 hidden sm:block mr-6 cursor-pointer hover:bg-gray-100 rounded-3xl  "
-          src="https://www.svgrepo.com/show/500410/notification.svg"
-          alt="notification_logo"
-        />
+        <IoIosNotificationsOutline className="w-8 h-8 " />
 
-        <img
-          className=" hidden sm:block h-10 w-10 mt-2 ml-11 "
-          src={USERACCOUTN}
-          alt="user_account"
-        />
+        <FaUserCircle className="w-8 h-8 " />
       </div>
     </div>
   );
